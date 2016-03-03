bdd962edfeef03cfac2773c55e05e65a409c67f1
Zachary Lipps <zlipps1@gmail.com>
2016-03-02 22:41:24 -0500

    Updated sleep call

--- TestApi/HomeScripts/recieveTemperature.py
+++ TestApi/HomeScripts/recieveTemperature.py
@@ -1,7 +1,7 @@
 import serial
 import requests
 import json
-import time
+from time import sleep
 
 ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
 ser.write("connected")
@@ -18,6 +18,6 @@ try:
 			out = ('{{"temperature": "{0}", "humidity": "{1}", "heatIndex": "{2}"}}').format(values[0], values[1], values[2])
 			print out
 			response = requests.post(url + '/api/temperature', data = out, headers = headers)
-            time.sleep(60)
+            sleep(60)
 except KeyboardInterrupt:
 	ser.close()
